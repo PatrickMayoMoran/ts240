61142c8f1d3373364fa7e2f6ee98efc4710729fb
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-11 10:25:22 -0700

    Add skeleton for convertToNumbers problem

---
+++ lesson1/src/convertToNumbers.ts
@@ -0,0 +1,7 @@
+const numbersInStringFormat: string[] = ["10", "20", "30", "40"];
+
+function convertToNumbers(arr: string[]): number[] {
+  // Your implementation here
+}
+
+console.log(convertToNumbers(numbersInStringFormat)); // [10, 20, 30, 40]
