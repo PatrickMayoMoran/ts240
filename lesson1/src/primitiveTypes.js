b1923849d974099e0ccb2c576966640c1cf6df95
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-11 09:50:27 -0700

    Add practice assigning variables to TypeScript primitives with type annotation

---
+++ lesson1/src/primitiveTypes.js
@@ -0,0 +1,17 @@
+// boolean
+var trueOrFalse = true;
+var isCompleted;
+isCompleted = true;
+// number
+var num = 1;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+// string
+var firstName = "Jane";
+var lastName = "Doe";
+var fullName = "".concat(firstName, " ").concat(lastName);
+// null
+var x = null;
+// undefined
+var y;
---
+++ lesson1/src/primitiveTypes.ts
@@ -0,0 +1,21 @@
+// boolean
+let trueOrFalse: boolean = true;
+let isCompleted: boolean;
+isCompleted = true;
+
+// number
+let num: number = 1;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+// string
+let firstName: string = "Jane";
+let lastName: string = "Doe";
+let fullName: string = `${firstName} ${lastName}`;
+
+// null
+let x: null = null;
+
+// undefined
+let y: undefined;
