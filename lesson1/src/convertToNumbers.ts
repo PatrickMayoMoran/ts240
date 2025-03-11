e9c0b2b89599b452a7f64edd0c21da5e34c0d134
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-11 10:32:09 -0700

    Add TypeScript implementation of convert to numbers function

--- lesson1/src/convertToNumbers.ts
+++ lesson1/src/convertToNumbers.ts
@@ -2,6 +2,7 @@ const numbersInStringFormat: string[] = ["10", "20", "30", "40"];
 
 function convertToNumbers(arr: string[]): number[] {
   // Your implementation here
+  return arr.map(n => parseInt(n, 10));
 }
 
 console.log(convertToNumbers(numbersInStringFormat)); // [10, 20, 30, 40]
