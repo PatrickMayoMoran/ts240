26c26be04330eaab375f52f49b92317b427c3973
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-12 11:57:24 -0700

    Add solution to pair problem asking for function definition with generic types

---
+++ lesson3/dist/pair.js
@@ -0,0 +1,6 @@
+"use strict";
+function pair(arg1, arg2) {
+    return [arg1, arg2];
+}
+const pairOfNumbers = pair(1, 2); // returns [1, 2]
+const pairOfStrings = pair("hello", "world"); // returns ["hello", "world"]
