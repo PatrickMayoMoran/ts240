7622f6f7727c32fd8cbba82871b0a8a3922a38f6
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-13 13:10:54 -0700

    Add type safe generic constraint version of accessing value for a property on an object

--- lesson4/dist/getProperty.js
+++ lesson4/dist/getProperty.js
@@ -1,10 +1,4 @@
 "use strict";
 function getProperty(obj, key) {
-    return obj[key]; // Error: No index signature with a parameter of type 'string' was found on type '{}'
+    return obj[key];
 }
-const obj = {
-    name: "John",
-    age: 30,
-};
-const x = getProperty(obj, "name");
-const y = getProperty(obj, "age");
--- lesson4/src/getProperty.ts
+++ lesson4/src/getProperty.ts
@@ -1,11 +1,3 @@
-function getProperty(obj: { [key: string]: unknown }, key: string) {
-  return obj[key]; // Error: No index signature with a parameter of type 'string' was found on type '{}'
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
 }
-
-const obj = {
-  name: "John",
-  age: 30,
-};
-
-const x = getProperty(obj, "name");
-const y = getProperty(obj, "age");
