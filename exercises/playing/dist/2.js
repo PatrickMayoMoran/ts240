6f7a3e57f7d44595f25350677d401d478d23f8f2
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-14 11:47:05 -0700

    Add solution to practice problem 2 asking for string typed array

---
+++ exercises/playing/dist/2.js
@@ -0,0 +1,4 @@
+"use strict";
+let colors = ['aqua', 'gelb', 'marron'];
+let moreColors = ['slate', 'purple', 'orange'];
+// colors.push(45); should throw error, argument of type number not assignable to parameter of type string
---
+++ exercises/playing/src/2.ts
@@ -0,0 +1,4 @@
+let colors: string[] = ['aqua', 'gelb', 'marron'];
+let moreColors: Array<string> = ['slate', 'purple', 'orange'];
+
+// colors.push(45); should throw error, argument of type number not assignable to parameter of type string
