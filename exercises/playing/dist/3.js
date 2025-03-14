0fba786c29f73d24545535b9065c6515f1fccca2
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-14 11:50:25 -0700

    Add solution to exercise 3 of typed function

---
+++ exercises/playing/dist/3.js
@@ -0,0 +1,6 @@
+"use strict";
+function greet(name, age) {
+    return `Hello! Your name is ${name} and your age is ${age}`;
+}
+console.log(greet("Mayo", 31));
+// console.log(greet("Ray", "31")); argument of type string is not assignable to parameter of type number
---
+++ exercises/playing/src/3.ts
@@ -0,0 +1,6 @@
+function greet(name: string, age: number): string {
+  return `Hello! Your name is ${name} and your age is ${age}`;
+}
+
+console.log(greet("Mayo", 31));
+// console.log(greet("Ray", "31")); argument of type string is not assignable to parameter of type number
