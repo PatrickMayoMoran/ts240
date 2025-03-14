6f400d659eccea8a1c60bf90ccb5247ebde1953b
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-14 12:35:02 -0700

    Add solution to exercise 7, testing inferred type when reassigning a variable

---
+++ exercises/playing/dist/7.js
@@ -0,0 +1,3 @@
+"use strict";
+let userID = 7;
+// userID = "Mayo"; -- error - inferred type of number "Type string is not assignable to type number"
---
+++ exercises/playing/src/7.ts
@@ -0,0 +1,2 @@
+let userID = 7;
+// userID = "Mayo"; -- error - inferred type of number "Type string is not assignable to type number"
