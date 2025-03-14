448d9b672b47179ad0ec082e32419b749b3d4a53
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-14 15:28:45 -0700

    Add example of object assignment to a variable being type checked against an interface

---
+++ exercises/blueprinting/dist/3.js
@@ -0,0 +1,7 @@
+"use strict";
+let myCar = {
+    make: "Honda",
+    model: "CRV",
+    year: 2011,
+    color: "red",
+};
---
+++ exercises/blueprinting/src/3.ts
@@ -0,0 +1,11 @@
+interface Car {
+  make: string;
+  model: string;
+  year: number;
+}
+
+let myCar: Car = {
+  make: "Honda",
+  model: "CRV",
+  year: 2011,
+}
