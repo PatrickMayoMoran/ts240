27b39aed21c3a3b7ba705cf1b5d31caedf2e73ff
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-13 17:19:52 -0700

    Add error catching version of async fetch problem

---
+++ lesson6/src/errorPromise.ts
@@ -0,0 +1,13 @@
+async function getData(url: string): Promise<void> {
+  try { 
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      console.log(e.message);
+    } else {
+      console.log("An unknown error occurred!");
+    }
+  }
+}
