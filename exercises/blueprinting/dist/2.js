9a67d29660b987d443366b16bf479be5f3ea8408
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-14 15:26:18 -0700

    Add solution for problem 2, having an object type annotation defined at variable initialization

---
+++ exercises/blueprinting/dist/2.js
@@ -0,0 +1,6 @@
+"use strict";
+let employee = {
+    id: 5,
+    name: "Mr. Potato",
+    department: "Ministry of Truth",
+};
---
+++ exercises/blueprinting/src/2.ts
@@ -0,0 +1,5 @@
+let employee: { id: number; name: string; department: string; } = {
+  id: 5,
+  name: "Mr. Potato",
+  department: "Ministry of Truth",
+}
