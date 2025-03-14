4b0e0867042fbb2e6e688c76336df25b18765dfd
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-14 09:44:48 -0700

    Add example of using type assertion to override TypeScript

--- lesson6/dist/valuesAtRuntime.js
+++ lesson6/dist/valuesAtRuntime.js
@@ -2,4 +2,5 @@
 const person = { name: "Jane", age: 45 };
 const personName = person;
 console.log(personName.name);
-console.log(personName.age); // type error - age not part of person name type
+// console.log(personName.age); type error - age not part of person name type
+console.log(personName.age); // type assertion to tell TypeScript we know more
--- lesson6/src/valuesAtRuntime.ts
+++ lesson6/src/valuesAtRuntime.ts
@@ -6,4 +6,5 @@ interface Person {
 const person: Person = { name: "Jane", age: 45 };
 const personName: Pick<Person, "name"> = person;
 console.log(personName.name);
-console.log(personName.age); // type error - age not part of person name type
+// console.log(personName.age); type error - age not part of person name type
+console.log((personName as any).age) // type assertion to tell TypeScript we know more
