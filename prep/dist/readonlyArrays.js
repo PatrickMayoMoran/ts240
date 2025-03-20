d7d56523433799256d3cf4575d7f054057b5f790
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-20 09:45:58 -0700

    Add examples of readonly arrays disallowing reassignment and push pop operations as well as functions returning readonly arrays

--- prep/dist/readonlyArrays.js
+++ prep/dist/readonlyArrays.js
@@ -1,4 +1,10 @@
 "use strict";
 let bTeam = ['Ben', 'Sultan', 'Luc'];
-bTeam[0] = 'frazer';
-bTeam.push('tim');
+// bTeam[0] = 'frazer'; no reassignment
+// bTeam.push('tim'); no push method
+function rosterSpot(roster) {
+    return roster.map((_, index) => index);
+}
+let bSpots = rosterSpot(bTeam);
+console.log(bSpots);
+// bSpots[0] = 'dan'; index signature in type readonly array<number> only permits reading
--- prep/src/readonlyArrays.ts
+++ prep/src/readonlyArrays.ts
@@ -2,5 +2,13 @@
 type Roster = ReadonlyArray<string>;
 
 let bTeam: Roster = ['Ben', 'Sultan', 'Luc'];
-bTeam[0] = 'frazer';
-bTeam.push('tim');
+// bTeam[0] = 'frazer'; no reassignment
+// bTeam.push('tim'); no push method
+
+function rosterSpot(roster: ReadonlyArray<string>): ReadonlyArray<number> {
+  return roster.map((_, index) => index);
+}
+
+let bSpots = rosterSpot(bTeam);
+console.log(bSpots);
+// bSpots[0] = 'dan'; index signature in type readonly array<number> only permits reading
