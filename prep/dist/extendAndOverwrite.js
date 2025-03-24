96bf19c29a57e5ab332651b11f69d3803eec564c
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-23 17:56:03 -0700

    Add example of intersection type compiling, until we try to assign something to a property with an intersection of never

--- prep/dist/extendAndOverwrite.js
+++ prep/dist/extendAndOverwrite.js
@@ -1,1 +1,4 @@
 "use strict";
+// let dschoey: EMS = {name: "Dschoey", station: 55};
+// type number unassignable to type never (!!)
+// type intersection said values have to satisfy both string and number, which can "never" happen, hence type
--- prep/src/extendAndOverwrite.ts
+++ prep/src/extendAndOverwrite.ts
@@ -12,3 +12,9 @@ interface Polizei {
 interface Paramedic extends FireFighter {
   // station: number; error - incorrect extension
 }
+
+type EMS = FireFighter & Polizei; // Type intersection will compile!
+
+// let dschoey: EMS = {name: "Dschoey", station: 55};
+// type number unassignable to type never (!!)
+// type intersection said values have to satisfy both string and number, which can "never" happen, hence type
