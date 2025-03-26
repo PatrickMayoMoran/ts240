1a7d4171f0d4e72c79fa00f56aa156a5d64038b4
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-03-25 23:07:10 -0700

    Add solution to string map practice problem

---
+++ prep/dist/stringMap.js
@@ -0,0 +1,5 @@
+"use strict";
+let dict = {
+    home: "where the heart is",
+    vacation: "Wherever I go!",
+};
---
+++ prep/src/stringMap.ts
@@ -0,0 +1,8 @@
+interface StringMap {
+  [index: string]: string;
+}
+
+let dict: StringMap = {
+  home: "where the heart is",
+  vacation: "Wherever I go!",
+}
